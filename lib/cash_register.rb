bc78b8b108e185d7c214721780331f8f4e0ee80a
Andrew Capp <acapp909@gmail.com>
2019-10-30 02:55:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount == 0
+    if discount == 0
       p "There is no discount to apply."
     else
       @total = @total.to_f * (100 - @discount.to_f) * 0.01
